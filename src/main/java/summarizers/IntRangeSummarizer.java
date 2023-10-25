b2ad3c5d7107ba99458dc724345354c2f8e6e47f
Brandon Pahla <phlbra004@cs.uct.ac.za>
2023-10-26 00:49:18 +0200

    Impl summarizeCollection method.
    This method converts a collection of integers to string summarized number ranges.

--- src/main/java/summarizers/IntRangeSummarizer.java
+++ src/main/java/summarizers/IntRangeSummarizer.java
@@ -4,6 +4,7 @@ import numberrangesummarizer.NumberRangeSummarizer;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 /**
@@ -14,10 +15,15 @@ import java.util.Collection;
 public class IntRangeSummarizer implements NumberRangeSummarizer {
 
     private final String inputDelimiter;
+    private final String outputElementDelimiter;
+    private final String outputGroupDelimiter;
 
     public IntRangeSummarizer() {
         this.inputDelimiter = ",";
+        this.outputGroupDelimiter = "-";
+        this.outputElementDelimiter = ", ";
     }
+
 
     /**
      * Collect the input.
@@ -43,6 +49,32 @@ public class IntRangeSummarizer implements NumberRangeSummarizer {
      * **/
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        return null;
+        if (input.isEmpty())
+            return "";
+
+        List<Integer> collection = new ArrayList<>(input);
+
+        StringBuilder result = new StringBuilder(String.valueOf(collection.get(0)));
+        int lastElement = collection.get(collection.size()-1);
+        String lastAdded = "";
+
+        for (int i = 1; i < input.size(); i++) {
+            int difference = Math.abs(collection.get(i - 1) - collection.get(i));
+            if (difference == 1) {
+                if (lastAdded.equals(this.outputGroupDelimiter))
+                    continue;
+                result.append(this.outputGroupDelimiter);
+                lastAdded = this.outputGroupDelimiter;
+                continue;
+            }
+            if (lastAdded.equals(this.outputGroupDelimiter))
+                result.append(collection.get(i - 1));
+            result.append(this.outputElementDelimiter).append(collection.get(i));
+            lastAdded = String.valueOf(collection.get(i));
+        }
+        if (lastAdded.equals(this.outputGroupDelimiter))
+            result.append(lastElement);
+
+        return result.toString();
     }
 }
--- src/test/java/summarizers/IntRangeSummarizerTest.java
+++ src/test/java/summarizers/IntRangeSummarizerTest.java
@@ -26,4 +26,66 @@ class IntRangeSummarizerTest {
         Collection<Integer> collection = intRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24");
         assertEquals(new ArrayList<>(Arrays.asList(1, 3, 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24)), collection);
     }
+
+    @Test
+    @DisplayName("summarize one element")
+    void summarizeCollectionOne() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        String summarized = intRangeSummarizer.summarizeCollection(new ArrayList<>(List.of(7)));
+        assertEquals("7", summarized);
+    }
+
+    @Test
+    @DisplayName("One Group")
+    void summarizeCollectionGroup(){
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        Collection<Integer> collection = intRangeSummarizer.collect("1,2,3,4,5,6,7,8,9,10");
+        String summarized = intRangeSummarizer.summarizeCollection(collection);
+        assertEquals("1-10", summarized);
+    }
+
+    @Test
+    @DisplayName("Last element not in a group")
+    void summarizeCollection() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        String summarized = intRangeSummarizer
+                .summarizeCollection(new ArrayList<>(Arrays.asList(1, 3, 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24, 31)));
+        assertEquals("1, 3, 6-8, 12-15, 21-24, 31", summarized);
+    }
+
+    @Test
+    @DisplayName("First element is a group")
+    void summarizeCollectionFirst() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        String summarized = intRangeSummarizer
+                .summarizeCollection(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 6, 8, 9, 10)));
+        assertEquals("1-4, 6, 8-10", summarized);
+    }
+
+    @Test
+    @DisplayName("Numbers in reverse")
+    void summarizeCollectionInReverse() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        Collection<Integer> collection = intRangeSummarizer.collect("31,24,23,22,21,15,14,13,12,8,7,6,3,1");
+        String summarized = intRangeSummarizer.summarizeCollection(collection);
+        assertEquals("31, 24-21, 15-12, 8-6, 3, 1", summarized);
+    }
+
+    @Test
+    @DisplayName("Numbers not in order")
+    void summarizeCollectionNotSorted() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        Collection<Integer> collection = intRangeSummarizer.collect("1,24,3,22,21,15,6,13,12,8,7,14,33,31");
+        String summarized = intRangeSummarizer.summarizeCollection(collection);
+        assertEquals("1, 24, 3, 22-21, 15, 6, 13-12, 8-7, 14, 33, 31", summarized);
+    }
+
+    @Test
+    @DisplayName("No groups")
+    void summarizeCollectionNoGroup() {
+        IntRangeSummarizer intRangeSummarizer = new IntRangeSummarizer();
+        Collection<Integer> collection = intRangeSummarizer.collect("2,4,6,8,10");
+        String summarized = intRangeSummarizer.summarizeCollection(collection);
+        assertEquals("2, 4, 6, 8, 10", summarized);
+    }
 }
